8a7d7005338cc0ce83f5f810a6c924b800a3ef7f
Mungualk <129359239+Mungualk@users.noreply.github.com>
2024-03-01 04:03:57 -0600

    p2 (chistoso) terminado
    se tarda mas haciendo el MathML que todo la función que resuelve, el problema:

--- Parcial 1/Tarea 1.7/p2.php
+++ Parcial 1/Tarea 1.7/p2.php
@@ -1,3 +1,11 @@
+<?php
+    function solve ($array)
+    {
+        $x = $array[0]; $y = $array[1]; $z = $array[2];
+        $r = ((($x+$y)/(2*$x)) + ((pow($x,3) + pow($y, 3))/(pow($x,2) + pow($y, 2))))/(pow($x,2) + pow($y,2) + pow($z,2));
+        return round($r,6);
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,7 +27,111 @@
         </div>
     </header>
     <div class="container">
-        
+        <div class="problema">
+            <div class="instrucciones">
+                <h2>Descripción</h2>
+                <p>Diseñar un programa que pregunte los valores de las variables x,y,z y evalue la función f(x,y,z)</p>
+                <div class="formula">   
+                    <math style="font-size: 1.7em" xmlns="http://www.w3.org/1998/Math/MathML">
+                        <mrow>
+                            <mrow>
+                                <mi>f</mi>
+                                <mo>(</mo>
+                                <mi>x</mi>
+                                <mi>, </mi>
+                                <mi>y</mi>
+                                <mi>, </mi>
+                                <mi>z</mi>
+                                <mo>)</mo>
+                                <mo>=</mo>
+                            </mrow>
+                            <mfrac>
+                                <mrow>
+                                    <mfrac>
+                                        <mrow>
+                                            <mi>x</mi>
+                                            <mo>+</mo>
+                                            <mi>y</mi>
+                                        </mrow>
+                                        <mrow>
+                                            <mn>2</mn>
+                                            <mi>x</mi>
+                                        </mrow>
+                                    </mfrac>
+                                    <mi>+</mi>
+                                    <mfrac>
+                                        <mrow>
+                                            <msup>
+                                                <mi>x</mi>
+                                                <mn>3</mn>
+                                            </msup>
+                                            <mi>+</mi>
+                                            <msup>
+                                                <mi>y</mi>
+                                                <mn>3</mn>
+                                            </msup>
+                                        </mrow>
+                                        <mrow>
+                                            <msup>
+                                                <mi>x</mi>
+                                                <mn>2</mn>
+                                            </msup>
+                                            <mi>+</mi>
+                                            <msup>
+                                                <mi>y</mi>
+                                                <mn>2</mn>
+                                            </msup>
+                                        </mrow>
+                                    </mfrac>
+                                </mrow>
+                            <mrow>
+                                <msup>
+                                    <mi>x</mi>
+                                    <mn>2</mn>
+                                </msup>
+                                <mi>+</mi>
+                                <msup>
+                                    <mi>y</mi>
+                                    <mn>2</mn>
+                                </msup>
+                                <mi>+</mi>
+                                <msup>
+                                    <mi>z</mi>
+                                    <mn>2</mn>
+                                </msup>
+                            <mrow>
+                            </mfrac>
+                        </mrow>
+                    </math>
+                </div>
+                <h2>Entrada</h2>
+                <p>El formulario pedirá tres números, uno por renglón, estos pueden ser positicos, negativos, cero e incluso pueden tener decimales.</p>
+                <h2>Salida</h2>
+                <p>La respuesta será el valor de la función F(x,y,z) redondeado a 6 decimales. <br>Nota: Se pueden usar funciones de redondeo para ajustar la salida (xd).</p>
+            </div>
+            <div class="formulario-p2">
+                <h2>Inserta datos:</h2>
+                <form action="/comoquieras/p2.php" method="post">
+                    <label for="x">X =</label>
+                    <input type="text" id="valor-x" name="valor-x" placeholder="1, 4, 7" required> <br>
+                    <label for="y">Y = </label>
+                    <input type="text" id="valor-y" name="valor-y" placeholder="2, 5, 8" required> <br>
+                    <label for="z">Z = </label>
+                    <input type="text" id="valor-z" name="valor-z" placeholder="3, 6, 9" required> <br>
+                    <input type="submit" value="enviar">
+                </form>
+                <div class="php-solve">
+                <?php
+                    if(isset($_POST['valor-x']) && isset($_POST['valor-y']) && isset($_POST['valor-z']))
+                    {
+                        $a = array($_POST['valor-x'], $_POST['valor-y'], $_POST['valor-z']);
+                        $r = solve($a);
+                        echo "<p>Valor final = {$r}</p>";
+                    }
+                ?>
+            </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
